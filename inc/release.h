738cfb68effabcef7b503da14a7eab100922d6f9
wangyue <yuew951115@qq.com>
2022-10-16 21:46:29 +0800

    compile libtdms

--- inc/release.h
+++ inc/release.h
@@ -1,4 +1,4 @@
 #define LIBTDMS_SVN_REV  "Unversioned directory"
 #define LIBTDMS_SVN_DIRTY "0"
-#define LIBTDMS_BUILD_TIME "2022-10-16 19:07:43"
-#define LIBTDMS_BUILD_ID "ubuntu-1665918463"
+#define LIBTDMS_BUILD_TIME "2022-10-16 21:45:42"
+#define LIBTDMS_BUILD_ID "ubuntu-1665927942"
